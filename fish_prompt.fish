02d9507b7ac632ac4937e853b2b777a91b8f8459
Hugo Haas <hugoh@hugoh.net>
2019-09-04 20:34:23 +0000

    Removed extra space

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -22,7 +22,7 @@ function fish_prompt
     set_color $fish_color_separator
     echo -n "@"
     set_color $fish_color_host
-    echo -n $__fish_prompt_hostname " "
+    echo -n $__fish_prompt_hostname
     set_color $fish_color_cwd
     echo -n (prompt_pwd)
     set_color $fish_color_separator
