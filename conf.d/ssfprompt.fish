35c8d2c5be971566830aa98d6165e0e9b56bd150
Hugo Haas <hugoh@hugoh.net>
2021-02-15 18:03:22 -0600

    Following fish recommended hierarchy

---
+++ conf.d/ssfprompt.fish
@@ -0,0 +1,12 @@
+set fish_prompt_pwd_dir_length 0
+
+set fish_color_user blue
+set fish_color_host green
+set fish_color_cwd cyan
+set fish_color_separator yellow
+set fish_color_separator_error red
+
+set __fish_git_prompt_show_informative_status 1
+set __fish_git_prompt_showcolorhints 'yes'
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showupstream 'auto'
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,16 +1,3 @@
-set fish_prompt_pwd_dir_length 0
-
-set fish_color_user blue
-set fish_color_host green
-set fish_color_cwd cyan
-set fish_color_separator yellow
-set fish_color_separator_error red
-
-set __fish_git_prompt_show_informative_status 1
-set __fish_git_prompt_showcolorhints 'yes'
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showupstream 'auto'
-
 function fish_prompt
     set -l last_status $status
     if set -q SSH_CONNECTION
@@ -32,8 +19,4 @@ function fish_prompt
     end
     echo -n "> "
     set_color normal
-end
-
-function fish_right_prompt
-  __fish_git_prompt "%s"
 end
